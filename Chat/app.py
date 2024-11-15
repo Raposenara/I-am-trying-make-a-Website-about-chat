3a25fd9d592ceb64343c2db5969214e8e19e824b
Hector <wakfufalso@gmail.com>
2024-11-15 07:31:33 -0300

    Configuração de porta para deploy no Render

--- Chat/app.py
+++ Chat/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'minha_chave_secreta'
@@ -15,4 +16,6 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    # Pegue a porta da variável de ambiente ou use a porta 5000 como padrão
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
